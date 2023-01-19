2074f9ecb817d62196396cce80d30c965e43449a
Miguel Angel Echeverri Quiroz <miguel.quiroz@gig-ext.com>
2023-01-19 23:03:04 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from "cors";
 import config from './config';
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.listen(config.port, () => {
     console.log(`Server listening on port: ${config.port}`)
