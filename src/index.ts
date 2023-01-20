102e325be8ef8e0660c1e703bb6398d558af0fbc
Miguel Angel Echeverri Quiroz <miguel.quiroz@gig-ext.com>
2023-01-20 02:18:16 +0100

    add register endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from "cors";
 import { port } from './config';
 import startDbConnection from './database/config';
+import authRouter from './routers/auth';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 startDbConnection();
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`)
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,5 +1,25 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { hash } from "bcryptjs";
+import User from "../database/entities/user";
 
 const router = Router();
+
+router.post("/register", async (request: Request, response: Response) => {
+
+    // Get data sent 
+    const {username, password} = request.body;
+
+    // Check it the username is already taken
+    let user = await User.findOne({ username });
+    if(user) return response.status(400).json({ error: "This username is already in use" });
+
+    // Save the new user
+    const hashedPassword = await hash(password, 10);
+    user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    // Send a response
+    response.status(201).json({ message: "Successful registration. Please login." });
+});
 
 export default router;
